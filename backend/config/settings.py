4d7865383f98057cc94b9fd3af36f9603b7a4753
steeve mocto <amoct@students.kennesaw.edu>
2026-04-27 17:10:49 -0400

    Feat: updated url

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -130,11 +130,11 @@ USE_TZ = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173", # Your React dev server
     "https://styledbymiah.vercel.app",
-    "https://styledbymiah-6s76okgv8-steeve06s-projects.vercel.app/",
+    "https://styledbymiah-6s76okgv8-steeve06s-projects.vercel.app",
 ]
 CSRF_TRUSTED_ORIGINS = [
     "https://styledbymiah.vercel.app",
-    "https://styledbymiah-6s76okgv8-steeve06s-projects.vercel.app/",
+    "https://styledbymiah-6s76okgv8-steeve06s-projects.vercel.app",
     "https://backend-styledbymiah.onrender.com"
 ]
 
