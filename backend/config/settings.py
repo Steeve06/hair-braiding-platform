9c4cfbc4d1f66d0c3789d2fff46da289fe035653
steeve mocto <amoct@students.kennesaw.edu>
2026-04-27 15:23:44 -0400

    Feat: added missing depencies 2

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = Tos.environ.get('DEBUG', 'False') == 'True'
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
