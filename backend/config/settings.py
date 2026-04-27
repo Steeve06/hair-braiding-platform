60444a5c0211a2fe7992973df14a6b1533fecd99
steeve mocto <amoct@students.kennesaw.edu>
2026-04-27 14:51:55 -0400

    FEAT: added some settngs.py lines

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
+    'cloudinary_storage',
+    'cloudinary',
     'corsheaders',
     'api',
 ]
