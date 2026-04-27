6c1e0e161cce113ed4ee4a323d5613f407976daf
steeve mocto <amoct@students.kennesaw.edu>
2026-04-27 14:59:50 -0400

    Feat: added missing depencies

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -175,3 +175,9 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
